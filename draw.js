9213cf494be0af6394ceb4f7ed452c1bf94bb523
Midas <eyeofmidas@gmail.com>
2013-10-28 13:12:38 -0400

    formatting PluginDraw code, added in dirty flags to reduce the number of context.strokeStyle and context.fillStyle calls made per frame, disabled calling reset after drawings (be careful of side effects now, call reset() yourself.)

--- draw.js
+++ draw.js
@@ -8,38 +8,52 @@ ig.module(
 PluginDraw = function(){
     var strokeColor = "#000000",
     lineWidth = 1,
-    fillColor = null;
+    fillColor = null,
+    strokeDirty = false,
+    fillDirty = false;
 
     this.setLineWidth = function(newLineWidth) {
-	lineWidth = newLineWidth;
+    	lineWidth = newLineWidth;
     };
 
     this.setStroke = function(newStrokeColor) {
-	strokeColor = newStrokeColor;
+    	if(strokeColor != newStrokeColor) {
+    		strokeDirty = true;
+    	}
+    	strokeColor = newStrokeColor;
     };
 
     this.setFill = function(newFillColor) {
-	fillColor = newFillColor;
+    	if(fillColor != newFillColor) {
+    		fillDirty = true;
+    	}
+    	fillColor = newFillColor;    		
     };
 
     this.rect = function(x, y, width, height) {
-	var context = ig.system.context;
-	var scale = ig.system.scale;
-	var scaledOffsetRect = {
-	    x: scale * x - scale * ig.game.screen.x,
-	    y: scale * y - scale * ig.game.screen.y,
-	    width: scale * width,
-	    height: scale * height
+		var context = ig.system.context;
+		var scale = ig.system.scale;
+		var scaledOffsetRect = {
+		    x: scale * x - scale * ig.game.screen.x,
+		    y: scale * y - scale * ig.game.screen.y,
+		    width: scale * width,
+		    height: scale * height
 	    };
         context.beginPath();
         context.rect(scaledOffsetRect.x, scaledOffsetRect.y, scaledOffsetRect.width, scaledOffsetRect.height);
         if (fillColor) {
-            context.fillStyle = fillColor;
+        	if(fillDirty) {
+        		context.fillStyle = fillColor;
+        		fillDirty = false;
+        	}
             context.fill();
         }
         
         if (strokeColor) {
-            context.strokeStyle = strokeColor;
+        	if(strokeDirty) {
+        		context.strokeStyle = strokeColor;
+        		strokeDirty = false;
+        	}
             context.lineWidth = lineWidth;
             context.stroke();
         }
@@ -47,24 +61,23 @@ PluginDraw = function(){
     };
 
     this.line = function(x1, y1, x2, y2) {
-	var context = ig.system.context;
-	var scale = ig.system.scale;
-	var startingVector = {
-		x: scale * x1 - scale * ig.game.screen.x,
-		y: scale * y1 - scale * ig.game.screen.y
-	    };
-	var endingVector = {
-		x: scale * x2 - scale * ig.game.screen.x,
-		y: scale * y2 - scale * ig.game.screen.y
-	    };
-        context.beginPath();
-        context.moveTo(startingVector.x, startingVector.y);
-        context.lineTo(endingVector.x, endingVector.y);
-
-        context.strokeStyle = strokeColor;
-        context.lineWidth = lineWidth;
-        context.stroke();
-		this.reset();
+		var context = ig.system.context;
+		var scale = ig.system.scale;
+		var startingVector = {
+			x: scale * x1 - scale * ig.game.screen.x,
+			y: scale * y1 - scale * ig.game.screen.y
+		    };
+		var endingVector = {
+			x: scale * x2 - scale * ig.game.screen.x,
+			y: scale * y2 - scale * ig.game.screen.y
+		    };
+	    context.beginPath();
+	    context.moveTo(startingVector.x, startingVector.y);
+	    context.lineTo(endingVector.x, endingVector.y);
+	
+	    context.strokeStyle = strokeColor;
+	    context.lineWidth = lineWidth;
+	    context.stroke();
     };
 
     this.point = function(x, y) {
@@ -87,16 +100,21 @@ PluginDraw = function(){
 		context.beginPath();
 		context.arc(centerX + ig.game.screen.x, centerY + ig.game.screen.y, scale * radius, 0, 2 * Math.PI, false);
 		if (fillColor) {
-            context.fillStyle = fillColor;
+			if(fillDirty) {
+				context.fillStyle = fillColor;
+				fillDirty = false;
+			}
             context.fill();
         }
         
         if (strokeColor) {
-            context.strokeStyle = strokeColor;
+        	if(strokeDirty) {
+        		context.strokeStyle = strokeColor;
+        		strokeDirty = false;
+        	}
             context.lineWidth = lineWidth;
             context.stroke();
         }
-        this.reset();
 	};
 };
 ig.draw = new PluginDraw();
